10495043b7e911ae6aeaad02426778e5e91185e8
Luiz Carlos Santana <112254950+luizcarlossantana@users.noreply.github.com>
2023-11-18 15:40:26 -0300

    criação do projeto

---
+++ api/src/test/java/com/PadroesDeProjetos/api/PadroesDeProjetosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PadroesDeProjetos.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadroesDeProjetosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
